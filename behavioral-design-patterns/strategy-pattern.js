6401c74989d37f2f5c3e9517769f5761dd8c10eb
Coskun Uyar <coskunuyarr@gmail.com>
2020-07-25 01:57:37 +0300

    stragety and template patterns are added.

---
+++ behavioral-design-patterns/strategy-pattern.js
@@ -0,0 +1,37 @@
+class Commute{
+    travel(transport){
+        return transport.travelTime();
+    }
+}
+
+class Vehicle{
+    travelTime(){
+        return this._timeTaken;
+    }
+}
+
+class Bus extends Vehicle{
+    constructor(){
+        super();
+        this._timeTaken = 10;
+    }
+}
+
+class Taxi extends Vehicle{
+    constructor(){
+        super();
+        this._timeTaken = 5;
+    }
+}
+
+class PersonalCar extends Vehicle{
+    constructor(){
+        super();
+        this._timeTaken = 3;
+    }
+}
+
+const commute = new Commute();
+
+console.log(commute.travel(new Taxi()));
+console.log(commute.travel(new Bus()));
---
+++ behavioral-design-patterns/template-pattern.js
@@ -0,0 +1,42 @@
+class Employee{
+    constructor(name,salary){
+        this._name = name;
+        this._salary = salary;
+    }
+
+    work(){
+        return `${this._name} handles ${this.responsibilities()}`;
+    }
+
+    getPaid(){
+        return `${this._name} got paid ${this._salary}`;
+    }
+}
+
+class Developer extends Employee{
+    constructor(name,salary){
+        super(name,salary);
+    }
+
+    responsibilities(){
+        return 'application development';
+    }
+}
+
+class Tester extends Employee{
+    constructor(name,salary){
+        super(name,salary);
+    }
+
+    responsibilities(){
+        return 'testing';
+    }
+}
+
+const dev = new Developer('Nathan',10000);
+console.log(dev.getPaid());
+console.log(dev.work());
+
+const tester = new Tester('Brian',90000);
+console.log(tester.getPaid());
+console.log(tester.work());
