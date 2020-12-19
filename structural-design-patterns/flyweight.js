cc58441f02504b4b9a27b60ba798127eb41f4bd3
coskunuyar <34946061+coskunuyar@users.noreply.github.com>
2020-12-19 22:32:32 +0300

    example revisited!

--- structural-design-patterns/flyweight.js
+++ structural-design-patterns/flyweight.js
@@ -1,34 +1,34 @@
-class Icecream {
-    constructor(flavour,price){
-        this.flavour = flavour;
-        this.price = price;
+// Structural Design Patterns
+// Flyweight pattern
+
+class DataManager{
+  constructor(){
+    this.fileNameCache = [];
+    this.fileCache = [];
+  }
+
+  async heavyNetworkRequest(fileName){
+    if(this.fileNameCache.includes(fileName)){
+      return new Promise((resolve , reject) => {
+        resolve(this.fileCache[this.fileNameCache.indexOf(fileName)])
+      })
+    }else{
+      return new Promise((resolve , reject) => {
+        setTimeout(() => {
+          const randomData = `${Date.now()} ${Math.random()}`
+          this.fileNameCache.push(fileName);
+          this.fileCache.push(randomData);
+          resolve(randomData);
+        },2000);
+      })
     }
+  }
 }
 
-class IcecreamFactory{
-    constructor(){
-        this._icecreams = [];
-    }
-
-    createIcecream(flavour,price){
-        let icecream = this.getIcecream(flavour);
-        if(icecream){
-            return icecream;
-        } else {
-            const newIcecream = new Icecream(flavour,price);
-            this._icecreams.push(newIcecream);
-            return newIcecream;
-        }
-    }
-    
-    getIcecream(flavour){
-        return this._icecreams.find(icecream => icecream.flavour === flavour);
-    }
-}
-
-const factory = new IcecreamFactory();
-
-const chocoVanilla = factory.createIcecream('chocolate and vanilla', 15);
-const vanillaChoco = factory.createIcecream('chocolate and vanilla', 15);
-
-console.log(chocoVanilla === vanillaChoco);
+const manager = new DataManager();
+manager.heavyNetworkRequest('example1.text').then((data) => {
+  console.log(data);
+})
+manager.heavyNetworkRequest('example1.text').then((data) => {
+  console.log(data);
+})
