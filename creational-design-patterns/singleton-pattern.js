4753b174e2fd7e2db198d11d62132667970ef1c5
coskunuyar <34946061+coskunuyar@users.noreply.github.com>
2020-12-19 14:52:16 +0300

    example is revisited!

--- creational-design-patterns/singleton-pattern.js
+++ creational-design-patterns/singleton-pattern.js
@@ -1,27 +1,24 @@
-class Mongoose{
-    constructor(data){
-        if(Mongoose.exists){
-            return Mongoose.instance;
-        }else{
-            this._data = data;
-            Mongoose.exists = true;
-            Mongoose.instance = this;
-            return Mongoose.instance;
-        }
-    }
-    
-    getData(){
-        return this._data;
-    }
+// Creational Design Patterns
+// Singleton Pattern
 
-    setData(data){
-        this._data = data;
-        return this._data;
-    }
+class Database{
+  constructor(data){
+    if(Database.instance) return Database.instance;
+    this._data = data;
+    Database.instance = this;
+  }
+
+  getData(){
+    return this._data;
+  }
+
+  setData(data){
+    this._data = data;
+    return this._data;
+  }
 }
 
-const dataBase = new Mongoose(["user 1","user 2","user 3"]);
-const dataBase2 = new Mongoose(["userlesss"]);
+const database1 = new Database([1,2,3,4]);
+const database2 = new Database([5,6,7,8,Math.random()]);
 
-console.log(dataBase.getData());
-console.log(dataBase2.getData());
+console.log(database1 === database2);
